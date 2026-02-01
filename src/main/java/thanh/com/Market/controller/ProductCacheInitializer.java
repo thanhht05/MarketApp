c37ce29dc33909899a9a700d65f1f572a2ff9a52
thanhht05 <huuthanhht05@gmail.com>
2026-02-01 22:50:15 +0700

    p

--- src/main/java/thanh/com/Market/controller/ProductCacheInitializer.java
+++ src/main/java/thanh/com/Market/controller/ProductCacheInitializer.java
@@ -1,9 +1,12 @@
 package thanh.com.Market.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import thanh.com.Market.domain.Product;
 import thanh.com.Market.domain.ProductCache;
 import thanh.com.Market.domain.ProductService;
 
@@ -15,7 +18,9 @@ public class ProductCacheInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        ProductCache.getInstance()
-                .loadFromDB(productService.getAll());
+        List<Product> list = productService.getAll();
+        System.out.println("INIT CACHE SIZE = " + list.size());
+
+        ProductCache.getInstance().loadFromDB(list);
     }
 }
