a34f218e909cb0b6c953ddb8be36ad7c3a83fc9a
thanhht05 <huuthanhht05@gmail.com>
2026-02-02 16:24:25 +0700

    h

--- src/main/java/thanh/com/Market/config/WebConfigure.java
+++ src/main/java/thanh/com/Market/config/WebConfigure.java
@@ -1,19 +1,20 @@
-package thanh.com.Market.config;
+// package thanh.com.Market.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfigure implements WebMvcConfigurer {
-    @Value("${upoadDir}")
-    private String uploadDir;
+// @Configuration
+// public class WebConfigure implements WebMvcConfigurer {
+// @Value("${upoadDir}")
+// private String uploadDir;
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**") // URL client gọi
-                .addResourceLocations(uploadDir); // Folder/file thật map với URL ciletn gọi
+// @Override
+// public void addResourceHandlers(ResourceHandlerRegistry registry) {
+// registry.addResourceHandler("/images/**") // URL client gọi
+// .addResourceLocations(uploadDir); // Folder/file thật map với URL ciletn gọi
 
-    }
-}
+// }
+// }
